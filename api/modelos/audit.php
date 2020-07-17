4b3548aa8ce719ccd5a82f425b1533b65b673d35
Ruben <rubenupc.rp@gmail.com>
2020-07-17 18:52:03 +0200

    Modelos principales de la aplicacion

---
+++ api/modelos/audit.php
@@ -0,0 +1,52 @@
+<?php
+
+class Audit{
+    private $id;
+    private $idTarea;
+    private $accion;
+    private $descripcion;
+    private $fecha;
+
+    public function __construct(){
+        $this->fecha = date("d-m-Y");
+    }
+
+    public function getId(){
+		return $this->id;
+	}
+ 
+
+    public function getIdTarea(){
+		return $this->idTarea;
+	}
+ 
+    public function setIdTarea($id){
+        $this->idTarea = $id;
+    }
+
+    public function getAccion(){
+		return $this->accion;
+	}
+ 
+    public function setAccion($accion){
+        $this->accion = $accion;
+    }
+
+    public function getDescripcion(){
+		return $this->descripcion;
+	}
+ 
+    public function setDescripcion($descripcion){
+        $this->descripcion = $descripcion;
+    }
+
+    public function getFecha(){
+		return $this->fecha;
+	}
+ 
+    public function setFechaCompletada($fecha){
+        $this->fecha = $fecha;
+    }
+}
+
+?>
---
+++ api/modelos/todo.php
@@ -0,0 +1,53 @@
+<?php
+
+class Todo{
+    private $id;
+    private $titulo;
+    private $completada;
+    private $fecha_creacion;
+    private $fecha_completada;
+
+    public function __construct(){
+        $this->completada = false;
+        $this->fecha_creacion = date("d-m-Y");
+    }
+
+    public function getId(){
+		return $this->id;
+	}
+ 
+
+    public function getTitulo(){
+		return $this->titulo;
+	}
+ 
+    public function setTitulo($titulo){
+        $this->titulo = $titulo;
+    }
+
+    public function getCompletada(){
+		return $this->completada;
+	}
+ 
+    public function setCompletada($completada){
+        $this->completada = $completada;
+    }
+
+    public function getFechaCreacion(){
+		return $this->fecha_creacion;
+	}
+ 
+    public function setFechaCreacion($fCreacion){
+        $this->fecha_creacion = $fCreacion;
+    }
+
+    public function getFechaCompletada(){
+		return $this->fecha_completada;
+	}
+ 
+    public function setFechaCompletada($fCompletada){
+        $this->fecha_completada = $fCompletada;
+    }
+}
+
+?>
